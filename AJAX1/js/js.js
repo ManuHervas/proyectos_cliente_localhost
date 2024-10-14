c52f2705fccb0b3ef6465473d329045a6c611c06
Manu <manuhrvs90@gmail.com>
2024-10-14 10:16:30 +0200

    primer ejercicio ajax con tablas y json

---
+++ AJAX1/js/js.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function () {
+  //Trate la plantilla
+  var contenedor = document.getElementById("contenedor");
+  var auxiliar = this.document.createElement("div");
+  var auxTbody = this.document.createElement("tbody");
+  var tabla;
+  fetch("./plantillas/tabla.html")
+    .then((respuesta) => respuesta.text())
+    .then((texto) => {
+      auxiliar.innerHTML = texto;
+      tabla = auxiliar.firstElementChild;
+      var tBody = tabla.tBodies[0];
+      auxTbody.appendChild(tBody.firstElementChild);
+      debugger;
+      var modelo = auxTbody.firstElementChild;
+      fetch("./datos/alumnos.json")
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          datos.forEach((element) => {
+            let fila = modelo.cloneNode(true);
+            fila.children[0].innerHTML = element.dni;
+            fila.children[1].innerHTML = element.nombre;
+            tBody.appendChild(fila);
+          });
+          contenedor.appendChild(tabla);
+        });
+    });
+});
