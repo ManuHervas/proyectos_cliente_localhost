699871ab2ee0ed7e32606d30766366dee8c663c4
ManuHervas <manuhrvs90@gmail.com>
2024-12-10 19:07:54 +0100

    eee

--- EJEMPLO_API/js/js1.js
+++ EJEMPLO_API/js/js1.js
@@ -1,45 +1,39 @@
-window.addEventListener("load", function(){
-    const btnMas = document.getElementById("mas");
-    //Cuando pulsamos el boton llamamos a traer nombres
-    btnMas.addEventListener("click", traerNombres);
+window.addEventListener("load", function () {
+  const btnMas = document.getElementById("mas");
+  //Cuando pulsamos el boton llamamos a traer nombres
+  btnMas.addEventListener("click", traerNombres);
 
-    function traerNombres(){
-        const nombre = document.getElementById("nombre").value;
-        if (nombre!="") {
-          //Creamos una peticion
-            var formulario = new FormData();
-            //Le declaramos el nombre que va a tener el metodo post y el valor 
-            formulario.append("nombre", nombre);
-            var peticion = new Request("php/api1.php",
-                {
-                    method : "POST",
-                    body: formulario,
-                });  
-        }else{
-            //Creamos una peticion
-        var peticion = new Request("php/api1.php",
-            {
-                method : "get"
-            }
-        );    
-        }
-        
-        //cuando tenemos los datos los procesamos en un json los utilizamos
-        fetch(peticion)
-            .then(respuesta=>respuesta.json())
-            .then(json=>{
-                const tbody = document.getElementById("nombres");
-                json.nombres.forEach(element => {
-                    var tr = document.createElement("tr");
-                    var td = document.createElement("td");
-
-                    td.innerHTML = element;
-                    tr.appendChild(td);
-                    tbody.appendChild(tr);
-                });
-            });
+  function traerNombres() {
+    const nombre = document.getElementById("nombre").value;
+    if (nombre != "") {
+      var formulario = new FormData();
+      //Le declaramos el nombre que va a tener el metodo post y el valor
+      formulario.append("nombre", nombre);
+      //Creamos una peticion//Creamos una peticion
+      var peticion = new Request("php/api1.php", {
+        method: "POST",
+        body: formulario,
+      });
+    } else {
+      //Creamos una peticion
+      var peticion = new Request("php/api1.php", {
+        method: "get",
+      });
     }
 
+    //cuando tenemos los datos los procesamos en un json los utilizamos
+    fetch(peticion)
+      .then((respuesta) => respuesta.json())
+      .then((json) => {
+        const tbody = document.getElementById("nombres");
+        json.nombres.forEach((element) => {
+          var tr = document.createElement("tr");
+          var td = document.createElement("td");
+
+          td.innerHTML = element;
+          tr.appendChild(td);
+          tbody.appendChild(tr);
+        });
+      });
+  }
 });
-
-
--- PONG_OBJ/js/index.js
+++ PONG_OBJ/js/index.js
@@ -1,34 +1,89 @@
-const tablero = document.getElementById("tablero");
+const container = document.getElementById("container");
+const canvas = document.getElementById("tablero");
+
+window.addEventListener("load", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
 class Raqueta {
   element;
   y = 0;
-  velocidad = 5;
+  velocidad = 10;
   movimiento;
 
-  constructor() {
+  constructor(yPosInicial) {
     this.element = document.createElement("div");
     this.element.classList = "raqueta";
-    tablero.appendChild(this.element);
+    this.y = yPosInicial;
+    this.element.style.top = this.y + "px";
+    container.appendChild(this.element);
   }
 
   subir() {
-      this.movimiento = setInterval(() => {        
-        this.y -= this.velocidad;
-        if (condition) {
-          
-        }
-        this.element.style.top = this.y + "px";
-      }, 20);
-  }
-
-  bajar() {
+    this.parar();
     this.movimiento = setInterval(() => {
-      this.y += this.velocidad;
+      this.y -= this.velocidad;
+      if (this.y < 0) {
+        this.y = 0;
+        this.parar();
+      }
       this.element.style.top = this.y + "px";
     }, 20);
   }
+
+  bajar() {
+    this.parar();
+    this.movimiento = setInterval(() => {
+      this.y += this.velocidad;
+      const limite = container.offsetHeight - this.element.offsetHeight;
+      if (this.y > limite) {
+        this.y = limite;
+        this.parar();
+      }
+      this.element.style.top = this.y + "px";
+    }, 20);
+  }
+
+  parar() {
+    clearInterval(this.movimiento);
+  }
+
+  parar() {
+    clearInterval(this.movimiento);
+  }
 }
 
-const j1 = new Raqueta();
-const j2 = new Raqueta();
+const j1 = new Raqueta(50);
+const j2 = new Raqueta(600);
+
+// Movimiento de las raquetas
+document.addEventListener("keydown", (ev) => {
+  switch (ev.code) {
+    case "ArrowUp":
+      j2.subir();
+      break;
+    case "ArrowDown":
+      j2.bajar();
+      break;
+    case "KeyW":
+      j1.subir();
+      break;
+    case "KeyS":
+      j1.bajar();
+      break;
+  }
+});
+
+document.addEventListener("keyup", (ev) => {
+  switch (ev.code) {
+    case "ArrowUp":
+    case "ArrowDown":
+      j2.parar();
+      break;
+    case "KeyW":
+    case "KeyS":
+      j1.parar();
+      break;
+  }
+});
