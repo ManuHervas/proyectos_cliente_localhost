1a65401ed118c823669e6d66f99127db6532e332
ManuHervas <manuhrvs90@gmail.com>
2024-12-11 18:54:04 +0100

    update

--- PONG_OBJ/js/index.js
+++ PONG_OBJ/js/index.js
@@ -1,6 +1,70 @@
 const container = document.getElementById("container");
 const canvas = document.getElementById("tablero");
 
+class Pelota {
+  element;
+  x = 0;
+  y = 0;
+  ancho = 30;
+  dx = -10;
+  dy = -10;
+  movimiento;
+
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.classList = "pelota";
+    container.appendChild(this.element);
+    this.resetPosition();
+    this.moverPelota();
+  }
+
+  resetPosition() {
+    this.x = canvas.width / 2;
+    this.element.style.left = this.x + "px";
+    this.y = canvas.height / 2;
+    this.element.style.top = this.y + "px";
+  }
+
+  moverPelota() {
+    if (!this.movimiento) {
+      this.movimiento = setInterval(() => {
+        //movimiento horizontal
+        this.x += this.dx;
+
+        if (
+          this.x < 0 + j1.ancho &&
+          this.y + this.ancho / 2 > j1.y &&
+          this.y + this.ancho / 2 < j1.y + j1.alto
+        ) {
+          this.dx = this.dx * -1;
+        }
+
+        if (
+          this.x + this.ancho > canvas.width - j2.ancho &&
+          this.y + this.ancho / 2 > j2.y &&
+          this.y + this.ancho / 2 < j2.y + j2.alto
+        ) {
+          this.dx = this.dx * -1;
+        }
+
+        //Meter punto
+        if (this.x < 0 || this.x > canvas.width - this.ancho) {
+          this.resetPosition();
+        }
+        this.element.style.left = this.x + "px";
+
+        //Movimiento vertical
+        this.y += this.dy;
+        if (this.y < 0 || this.y > canvas.height - this.ancho) {
+          this.dy = this.dy * -1;
+          console.log(this.dy);
+        }
+        this.element.style.top = this.y + "px";
+      }, 20);
+    }
+  }
+}
+
 window.addEventListener("load", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -11,6 +75,8 @@ class Raqueta {
   y = 0;
   velocidad = 10;
   movimiento;
+  ancho = 20;
+  alto = 140;
 
   constructor(yPosInicial) {
     this.element = document.createElement("div");
@@ -56,8 +122,8 @@ class Raqueta {
 
 const j1 = new Raqueta(50);
 const j2 = new Raqueta(600);
+const pelota = new Pelota(800, 400);
 
-// Movimiento de las raquetas
 document.addEventListener("keydown", (ev) => {
   switch (ev.code) {
     case "ArrowUp":
